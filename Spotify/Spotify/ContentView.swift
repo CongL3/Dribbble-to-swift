45227539afbdc5448872f1699717a26403d795bc
CongL3 <congle.11@gmail.com>
2021-05-08 03:31:10 +0700

    Adds Spotify

---
+++ Spotify/Spotify/ContentView.swift
@@ -0,0 +1,144 @@
+//
+//  ContentView.swift
+//  Spotify
+//
+//  Created by Cong Le on 08/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+	
+	@State var selectedTab = "house.fill"
+	@State var volume: CGFloat = 0
+	@State var showSideBar: Bool = false
+	
+    var body: some View {
+		HStack(spacing: 0) {
+			
+			let lightWhite = Color.white.opacity(0.7)
+			
+			// side bar
+			VStack {
+				
+				Image("logo")
+					.resizable()
+					.aspectRatio(contentMode: .fill)
+					.frame(width: 55, height: 55)
+					.padding(.top)
+				
+				VStack {
+					TabButton(image: "house.fill", selectedTab: $selectedTab)
+					TabButton(image: "safari.fill", selectedTab: $selectedTab)
+					TabButton(image: "mic.fill", selectedTab: $selectedTab)
+					TabButton(image: "clock.fill", selectedTab: $selectedTab)
+				}
+				.frame(height: getRect().height / 2.3)
+				.padding(.top, 10)
+				
+				Spacer(minLength: 50)
+	
+				Button(action: {
+					volume = volume + 0.1 < 1 ? volume + 0.1 : 1
+				}, label: {
+					Image(systemName: "speaker.wave.2.fill")
+						.font(.title2)
+						.foregroundColor(.white)
+				})
+				
+				GeometryReader { proxy in
+
+					let height = proxy.frame(in: .global).height
+					let progress = height * volume
+
+					ZStack (alignment: .bottom){
+						
+						Capsule()
+							.fill(Color.gray.opacity(0.5))
+							.frame(width: 4)
+						
+						Capsule()
+							.fill(Color.white)
+							.frame(width: 4, height: progress)
+
+					}
+					.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+					.padding(.vertical, 10)
+
+				}
+
+				Button(action: {
+					volume = volume - 0.1 > 0 ? volume - 0.1 : 0
+				}, label: {
+					Image(systemName: "speaker.wave.1.fill")
+						.font(.title2)
+						.foregroundColor(.white)
+				})
+				
+				Button(action: {
+//					withAnimation(.easeIn) {
+//						showSideBar.toggle()
+//					}
+//
+				}, label: {
+					Image(systemName: "chevron.left")
+						.font(.title2)
+						.foregroundColor(lightWhite)
+				})
+				.padding(.top, 10)
+				.padding(.bottom, getSafeArea().bottom == 0 ? 15 : 0)
+//				.offset(x: 100)
+
+			}
+			// max side bar width
+			.frame(width: 80)
+			.background(Color.black.ignoresSafeArea())
+//			.offset(x: showSideBar ? 0 : -100)
+			
+			// reclaiming the spacing by using negative spacing
+//			.padding(.trailing, showSideBar ? 0 : -100)
+			
+			// main content
+			ScrollView(showsIndicators: false, content: {
+				
+			})
+			.background(Color("bg").ignoresSafeArea())
+		}
+	
+	
+	}
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct TabButton: View {
+	
+	var image: String
+	@Binding var selectedTab: String
+	
+	var body: some View {
+		
+		Button(action: {
+			withAnimation{selectedTab = image}
+		}, label: {
+			Image(systemName: image)
+				.font(.title2)
+				.foregroundColor(selectedTab == image ? .white : .gray.opacity(0.6))
+				.frame(maxHeight: .infinity)
+		})
+	}
+}
+
+extension View {
+	func getRect() -> CGRect {
+		return UIScreen.main.bounds
+	}
+
+	func getSafeArea() -> UIEdgeInsets {
+		return UIApplication.shared.windows.first?.safeAreaInsets ?? UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+	}
+}
---
+++ Spotify/Spotify/Song.swift
@@ -0,0 +1,41 @@
+//
+//  Song.swift
+//  Spotify
+//
+//  Created by Cong Le on 08/05/2021.
+//
+
+import SwiftUI
+
+struct Song: Identifiable {
+	
+	var id = UUID().uuidString
+	var albumName : String
+	var albumAuthor : String
+	var albumCover : String
+}
+
+var recentlyPlayed = [
+
+	Song(albumName: "Privacy", albumAuthor: "Cardi B", albumCover: "p1"),
+	Song(albumName: "Square Shap", albumAuthor: "Dino man", albumCover: "p2"),
+	Song(albumName: "Starboy", albumAuthor: "Star Man", albumCover: "p3"),
+	Song(albumName: "Pink", albumAuthor: "Black pink", albumCover: "p4")
+]
+
+var likedSongs = [
+	
+	Song(albumName: "Privacy", albumAuthor: "Cardi B", albumCover: "p5"),
+	Song(albumName: "Square Shap", albumAuthor: "Dino man", albumCover: "p6"),
+	Song(albumName: "Starboy", albumAuthor: "Star Man", albumCover: "p7"),
+	Song(albumName: "Pink", albumAuthor: "Black pink", albumCover: "p8"),
+	Song(albumName: "Privacy", albumAuthor: "Cardi B", albumCover: "p9"),
+	Song(albumName: "Square Shap", albumAuthor: "Dino man", albumCover: "p10"),
+	Song(albumName: "Starboy", albumAuthor: "Star Man", albumCover: "p11"),
+	Song(albumName: "Pink", albumAuthor: "Black pink", albumCover: "p12"),
+	Song(albumName: "Pink", albumAuthor: "Black pink", albumCover: "p13"),
+	Song(albumName: "Pink", albumAuthor: "Black pink", albumCover: "p14"),
+	Song(albumName: "Pink", albumAuthor: "Black pink", albumCover: "p15")
+]
+
+var genre = ["Classic", "Hip-Hop", "Electronic", "Chilout", "Dark", "Clam", "Dance", "Ambient"]
